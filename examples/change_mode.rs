d16a2930746848baa9a97f6d4d3ff50ca4f2dc03
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-11-07 18:06:13 +0100

    Fix example code

--- examples/change_mode.rs
+++ examples/change_mode.rs
@@ -1,6 +1,6 @@
 use alarmate::{Area, Client, Mode};
 
-#[runtime::main(runtime_tokio::Tokio)]
+#[tokio::main]
 async fn main() -> alarmate::Result {
     let mut client = Client::new("admin", "changeme", "192.168.178.10".parse().unwrap());
     client.change_mode(Area::Area1, Mode::Disarmed).await?;
