ac9414994cc175bd0f6920cdda81f83daf31e643
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-04-20 18:58:49 +0200

    Add CI workflow

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@
 //! To get started, we need to create a client:
 //!
 //! ```rust
-//!   let client = alarmate::Client::new("admin", "changeme", "192.168.178.10");
+//!   let ip_address = "192.168.178.10".parse().unwrap();
+//!   let client = alarmate::Client::new("admin", "changeme", ip_address);
 //! ```
 
 #![deny(missing_docs)]
