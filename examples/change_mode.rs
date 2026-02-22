29544eaaf15cfb85761d8fc52a7140a85caa8290
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2026-02-22 22:34:38 +0100

    Upgrade reqwest to 0.13 and refactor Client

--- examples/change_mode.rs
+++ examples/change_mode.rs
@@ -2,7 +2,7 @@ use alarmate::{Area, Client, Mode};
 
 #[tokio::main]
 async fn main() -> alarmate::Result {
-    let mut client = Client::new("admin", "changeme", "192.168.178.10".parse().unwrap());
-    client.change_mode(Area::Area1, &Mode::Disarmed).await?;
+    let mut client = Client::new("admin", "changeme", "192.168.178.10".parse().unwrap())?;
+    client.change_mode(Area::Area1, Mode::Disarmed).await?;
     Ok(())
 }
